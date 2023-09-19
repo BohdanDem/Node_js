fb7288653784562d60114c37dd61032215a4f581
BohdanDem <bogdandemchuk.1@gmail.com>
2023-09-19 22:54:31 +0300

    HomeWork_v2_basics

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const fs = require ('node:fs');
 const path = require ('node:path')
 
-//const filePath = path.join(__dirname, 'coreFolder', 'file_5.txt')
+const itemPath = path.join(__dirname, 'coreFolder')
 const filePath = path.join(__dirname, 'coreFolder')
 
-//fs.mkdir(filePath, (err) => {})
+for (let i = 1; i < 6; i++) {
+  fs.mkdir(path.join(itemPath, 'folder_' + i), {recursive: true}, (err) => {})
+}
 
-// fs.writeFile(filePath, 'Hello World!', (err) => {
-//   if (err) throw new Error(err.message);
-// })
+for (let i = 1; i < 6; i++) {
+  fs.writeFile(path.join(itemPath, `file_${i}.txt`), 'Hello World!', (err) => {})
+}
 
 fs.readdir(filePath, {withFileTypes: true}, function(err, items) {
   for (let i= 0; i < items.length; i ++) {
-    if (items[i].isDirectory()) {
-      console.log('Folder:', items[i].name);
-    }else if (items[i].isFile()) {
-      console.log('File:', items[i].name);
-    }
+    console.log(items[i].isDirectory() ? 'Folder:' : 'File:', items[i].name)
   }
 });

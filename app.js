65e3d69e9c6a0ecf63cbce629e514b874172583f
BohdanDem <bogdandemchuk.1@gmail.com>
2023-09-24 22:25:18 +0300

    HomeWork_2_added_changes

--- app.js
+++ app.js
@@ -1,55 +1,101 @@
 const express = require('express');
+
 const app = express();
-const path = require("node:path");
-const fs = require("node:fs")
+const fsService = require('./fs.service')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const usersDataBase = path.join(__dirname, 'db.json')
-let users = [];
+app.get('/users', async (req, res)=> {
+    const users = await fsService.reader()
+    res.json(users)
+})
 
-fs.readFile(usersDataBase, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
+app.get('/users/:id', async (req, res)=> {
+    try {
+        const { id } = req.params;
+        const users = await fsService.reader()
+        const user = users.find(user => user.id === (+id))
+        if (!user) {
+            throw new Error('User does not exist')
+        }
+        res.json(user)
+    }catch (e) {
+        res.status(404).json(e.message);
     }
-    users = JSON.parse(data)
-});
-
-app.get('/users', (req, res)=>{
-    res.json({data: users})
 })
 
-app.get('/users/:id', (req, res)=>{
-    const { id } = req.params;
-    res.json({data: users[+id - 1]})
+app.post('/users', async (req, res)=>{
+    try {
+        const { name, age, email } = req.body
+
+        if (!name || name.length < 2) {
+            throw new Error('The name is too small')
+        }
+        if (!age || age < 1 || age > 120) {
+            throw new Error('The age should be in range from 1 to 120')
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('The email is wrong')
+        }
+
+        const users = await fsService.reader()
+        const lastId = users[users.length - 1].id
+        const newUser = { id: lastId + 1, name, age, email }
+
+        users.push(newUser)
+        await fsService.writer(users)
+        res.status(201).json({message: "User created"});
+    }catch (e) {
+        res.status(404).json(e.message);
+    }
 })
 
-app.post('/users', (req, res)=>{
-    users.push(req.body)
-    const updatedUsers = JSON.stringify(users)
-    fs.writeFile(path.join(usersDataBase), updatedUsers, () => {});
-    res.status(201).json({message: "User created"});
+app.put('/users/:id', async (req, res)=>{
+    try {
+        const { id } = req.params;
+        const { name, age, email } = req.body;
+
+        if (!name || name.length < 2) {
+            throw new Error('Name is too small')
+        }
+        if (!age || age < 1 || age > 120) {
+            throw new Error('The age should be in range from 1 to 120')
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('The email is wrong')
+        }
+
+        const users = await fsService.reader()
+        const user = users.find(user => user.id === (+id))
+        user.name = name
+        user.age = age
+        user.email = email
+
+        await fsService.writer(users)
+        res.status(201).json({message: 'User is updated'})
+    }catch (e) {
+        res.status(404).json(e.message);
+    }
 })
 
-app.put('/users/:id', (req, res)=>{
-    const { id } = req.params;
-    const nUsers = users.map(user => (user.id === id) ? req.body : user)
+app.delete('/users/:id', async (req, res)=>{
+    try {
+        const { id } = req.params;
 
-    console.log(nUsers);
+        const users = await fsService.reader()
+        const index = users.findIndex(user => user.id === (+id))
 
-    const updUsers = JSON.stringify(nUsers)
-    fs.writeFile(path.join(usersDataBase), updUsers, () => {});
+        if (index === -1) {
+            throw new Error('User does not exist')
+        }
 
-    res.json({message: 'User updated'})
-})
-
-app.delete('/users/:id', (req, res)=>{
-    const { id } = req.params;
-    users.splice(+id - 1, 1);
-
-    res.sendStatus(204);
+        users.splice(index, 1)
+        await fsService.writer(users)
+        res.sendStatus(204);
+    }catch (e) {
+        res.status(404).json(e.message);
+    }
 })
 
 const PORT = 5000;

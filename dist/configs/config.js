e27394c3c82bad835264c7c2fca5c9a6696b037e
BohdanDem <bogdandemchuk.1@gmail.com>
2023-09-27 10:15:06 +0300

    HomeWork_3_ts

--- dist/configs/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configs = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.configs = {
-    DB_URI: process.env.DB_URI,
-};

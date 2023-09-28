663d763c6cbb13eaf30fcf75242bd9977440c3d3
BohdanDem <bogdandemchuk.1@gmail.com>
2023-09-28 08:50:16 +0300

    master_typeScript

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,15 @@ import * as mongoose from "mongoose";
 import { configs } from "./configs/config";
 
 const app = express();
+const fsService = require('./fs.service')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/users', async (req, res)=> {
+  const users = await fsService.reader()
+  res.json(users)
+})
 
 const PORT = 5000;
 

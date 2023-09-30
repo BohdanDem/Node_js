0cf2e66998c02e12b66aa7ddd0408d47a32063ad
BohdanDem <bogdandemchuk.1@gmail.com>
2023-09-30 16:49:16 +0300

    HomeWork_3_v2_ts added middleware

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { userController } from "../controllers/user.controller";
 import { commonMiddleware } from "../middlewares/common.middleware";
 import { userMiddleware } from "../middlewares/user.middleware";
+import { UserValidator } from "../validators/user.validator";
 
 const router = Router();
 
@@ -10,15 +11,24 @@ router.get("", userController.getAll);
 
 router.get(
   "/:id",
-  commonMiddleware.isIdValid,
+  commonMiddleware.isIdValid("id"),
   userMiddleware.getByIdOrThrow,
   userController.getById,
 );
 
-router.post("/", userController.post);
+router.post(
+  "/",
+  commonMiddleware.isBodyValid(UserValidator.create),
+  userController.post,
+);
 
-router.delete("/:id", userController.delete);
+router.delete("/:id", commonMiddleware.isIdValid("id"), userController.delete);
 
-router.put("/:id", userController.put);
+router.put(
+  "/:id",
+  commonMiddleware.isIdValid("id"),
+  commonMiddleware.isBodyValid(UserValidator.update),
+  userController.put,
+);
 
 export const userRouter = router;

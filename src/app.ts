6738320e6a23d0894258859e399a19e8765deb9d
BohdanDem <bogdandemchuk.1@gmail.com>
2023-10-12 12:35:33 +0300

    Lesson_8_cron_swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import * as mongoose from "mongoose";
+import * as swaggerUi from "swagger-ui-express";
 
+import { cronRunner } from "./ crons";
 import { configs } from "./configs/config";
 import { ApiError } from "./errors/api.error";
 import { authRouter } from "./routers/auth.router";
 import { carRouter } from "./routers/car.router";
 import { userRouter } from "./routers/user.router";
+import * as swaggerJson from "./utils/swagger.json";
 
 const app = express();
 
@@ -15,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
 app.use("/cars", carRouter);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.use((error: ApiError, req: Request, res: Response, next: NextFunction) => {
   const status = error.status || 500;
@@ -28,5 +32,6 @@ const PORT = 5000;
 
 app.listen(PORT, async () => {
   await mongoose.connect(configs.DB_URI);
+  cronRunner();
   console.log(`Server has successfully started on PORT ${PORT}`);
 });
